10e872e53d04ca8ffe8a185031373d8f0b41ce21
hannasamuel20 <hanna17st@gmail.com>
2020-05-07 21:42:58 +0300

    Updates

---
+++ precache-manifest.cf494f7fd94fe98e559bf9916cd22b44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05cefa05dffd94b003693388c6fa9a06",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "779ff22cafa15b7d0add",
+    "url": "/monsters-rolodex/static/css/main.4bc4cfee.chunk.css"
+  },
+  {
+    "revision": "b97bf220b33e6d1d596e",
+    "url": "/monsters-rolodex/static/js/2.052edde3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.052edde3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "779ff22cafa15b7d0add",
+    "url": "/monsters-rolodex/static/js/main.8b6afbb1.chunk.js"
+  },
+  {
+    "revision": "5b9aba0148f86be6c127",
+    "url": "/monsters-rolodex/static/js/runtime-main.ecb0db4c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
